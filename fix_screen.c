ba8f1be7db6fd4c867d22d63f6942842e9b794d0
Joe Meszar <github@outk.st>
2016-09-25 12:15:00 -0400

    Added fix_screen.c
    This helper function will reset the ICANON and ECHO flags for the
    current terminal.

---
+++ fix_screen.c
@@ -0,0 +1,20 @@
+#include <termios.h>	/* TCGETS TCSETS */
+#include <sys/ioctl.h>	/* ioctl() */
+
+/*
+	Fixes the terminal settings back to allow ICANON and ECHO.
+
+	These flags give the terminal an interactive view to the user, and
+	when switched off the terminal does not show what the user has typed
+	(no ECHO) and does not require the ENTER key to be used to send input
+	to the device (no ICANON).
+*/
+int main() {
+	struct termios terminal_settings;
+	ioctl(1, TCGETS, &terminal_settings);	// get terminal settings
+	terminal_settings.c_lflag |= ICANON;	// set ICANON flag
+	terminal_settings.c_lflag |= ECHO;		// set ECHO flag
+	ioctl(1, TCSETS, &terminal_settings);	// set terminal settings
+
+	return 0;
+}
